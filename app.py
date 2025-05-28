f6bbb81a06ffaea7948a5bb93bd3273e30061e70
aminnameni <AMIN.NAMENY@GMAIL.COM>
2025-05-27 20:45:54 -0400

    app.py

--- app.py
+++ app.py
@@ -64,7 +64,6 @@ except Exception as e:
     print("❗️ خطای کامل:\n", err)
     return f"<pre>⚠️ خطای سرور:\n\n{err}</pre>"
 
-
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
